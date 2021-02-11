058c633b1c8445df39c2fada9988e196d08185e3
Sangpil Jung <tankteam20@gmail.com>
2021-02-12 00:50:36 +1000

    add navbar raw design

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css'
+
+class Navbar extends React.Component{
+    render(){
+        return (<div>
+            <div className="navbar">
+            <button>✘</button>
+            <h1>helloworld</h1>
+            </div>
+        </div>)
+    }
+}
+
+export default Navbar;
